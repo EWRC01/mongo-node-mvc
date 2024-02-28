7e9adf9e0e534c77bdce54dc95e7db38a793d7b9
EWRC01 <edwin1026@hotmail.es>
2024-02-28 11:46:15 -0600

    Add middleware to protect some routes

---
+++ src/api/middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+function verifyToken(req, res, next) {
+    const token = req.header('Authorization');
+    if(!token) return res.status(401).json({error: 'Access Denied'});
+
+    try {
+        const decoded = jwt.verify(token, 'secret');
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        res.status(401).json({error: 'Invalidad Token'});
+    }
+};
+
+module.exports = verifyToken;
